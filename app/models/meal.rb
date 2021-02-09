ef9d6f8ec6eb4aca73bdce9c03c3d12deb5abad9
Adriana <am20dipi@alum.siena.edu>
2021-02-09 15:06:09 -0500

    add validations to models

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,7 @@
 class Meal < ActiveRecord::Base
     belongs_to :user
+    validates :name, :content, :type, presence: true
+    
     
     # a meal has a name, content, and a type
     # validates name, content, type; has presence
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
     # A macro that alerts ActiveRecord of the relationship between two Models; provides methods from ActiveRecord associations
     has_secure_password
     # A macro that provides methods such as: .authenticate
-
+    validates :name, :email, :password, presence: true
     # a user has a name, an email, a password
     #validates uniqueness email
     #validates password confirmation: true
-    
+
 end
