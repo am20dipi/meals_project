14c6fed9d28e4104c2aeaae55f757366d1591cbb
Adriana <am20dipi@alum.siena.edu>
2021-02-09 17:35:35 -0500

    add helper methods to routes in meals controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -52,7 +52,10 @@ class MealsController < ApplicationController
     end
 
     patch '/meals/:id' do 
-        meal = Meal.find_by(id: params[:id])
+        # comes from Middleware Rack::MethodOverride; 
+        # it is looking for key "_method"; if there is one in params, the value associated with the key is PATCH.
+        meal = current_user.meals.find_by(id: params[:id])
+        
         if meal.update(name: params[:name], content: params[:content], type: params[:type]) #checking to see if meal exists and has presence in its validations 
             redirect "/meals/#{meal.id}"
         else
@@ -64,23 +67,30 @@ class MealsController < ApplicationController
 
     # SHOW
     get '/meals/:id' do # :id adds the key in the params hash; the number after the colon becomes the key in the hash
-        @meal = Meal.find_by(id: params[:id])
+        if logged_in?
+            @meal = current_user.meals.find_by(id: params[:id])
 
-        if @meal #if a meal exists, then render the show view.
-            erb :'meals/show'
-        else #if not, then redirect back to index.
-            redirect "/meals"
+            if @meal #if a meal exists, then render the show view.
+                erb :'meals/show'
+            else #if not, then redirect back to index.
+                redirect "/meals"
+            end
+        else
+            redirect "login"
         end
-
     end
 
 
 
     # DELETE
     delete '/meals/:id' do 
-        @meal = Meal.find_by(id: params[:id])
-        @meal.destroy
-        redirect "/meals"
+        if logged_in?
+            @meal = current_user.meals.find_by(id: params[:id])
+            @meal.destroy
+            redirect "/meals"
+        else
+            redirect "/login"
+        end
     end
 
 
