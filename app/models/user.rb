86311b5ff6f59482de42986f147e9c0e6fc79b30
Adriana <am20dipi@alum.siena.edu>
2021-02-19 17:02:58 -0500

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ActiveRecord::Base
     # A macro that alerts ActiveRecord of the relationship between two Models; provides methods from ActiveRecord associations
     has_secure_password
     # A macro that provides methods such as: .authenticate. ActiveRecord is working with BCrypt.
+    validates :password, length: {in: 4..30}, confirmation: true 
+    # checks to see if password matches the primary password input
     validates :name, :email, :password, presence: true
     # a user has a name, an email, a password
     #validates password confirmation: true
-    validates_uniqueness_of :email
-    #validates uniqueness email
+    validates :email, uniqueness: true, length: {in: 5...30}
+    #validates uniqueness of email, and sets min/max of characters
 end
